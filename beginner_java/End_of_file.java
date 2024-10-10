d1e264ad275d05caf846520d5a09457c4ae939f9
Dat09 <126665227+johhnydeep3721@users.noreply.github.com>
2024-10-10 10:13:36 +0700

    java

---
+++ beginner_java/End_of_file.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class End_of_file {
+    public static void main(String[] args) {
+         
+        
+    }
+}
--- beginner_java/date_and_time.java
+++ beginner_java/date_and_time.java
@@ -1,4 +1,4 @@
-package beginner_java;
+
 import java.util.Date;
 public class date_and_time {
     public static void main(String[] args) {
